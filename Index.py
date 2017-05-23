9ef0dd56c92c23d7fbf1c292398a495f584876a2
stories2 <stories2@naver.com>
2017-05-24 06:36:08 +0900

    Crawl Module : find what u want from homepage
    홈페이지에서 원하는 키워드의 내용이 나온 제목들만 가져오는 기능 추가

--- Index.py
+++ Index.py
@@ -1,8 +1,22 @@
+#-*- coding: utf-8 -*-
 from selenium import webdriver
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 binaryPath = "./files/chromedriverMac"
 
 chromeWebBrowser = webdriver.Chrome(binaryPath)
 
-chromeWebBrowser.get("http://www.naver.com")
+chromeWebBrowser.get("http://mma.go.kr/board/boardList.do?mc=usr0000053&gesipan_id=16")
+noticeList = chromeWebBrowser.find_element_by_xpath('//table//tbody')
+noticeListDatas = noticeList.find_elements_by_xpath("tr")
+# print noticeListDatas
+count = 0
+for eachNoticeData in noticeListDatas:
+    count += 1
+    eachNoticeString = eachNoticeData.text
+    if eachNoticeString.find("서울") != -1 and eachNoticeString.find("사회복무요원") != -1:
+        print "#", count, eachNoticeString
 chromeWebBrowser.quit()
