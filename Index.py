32d8f0b0263383b09514cf5ae1581bd5f66c5fe1
stories2 <stories2@naver.com>
2017-05-24 10:04:17 +0900

    Crawl Module : refactoring crawler
    크롤링 해온 데이터 정리 및 각 플렛폼 지원

--- Index.py
+++ Index.py
@@ -1,22 +1,61 @@
 #-*- coding: utf-8 -*-
 from selenium import webdriver
+from Setting import DefineManager
 import sys
+
+def InitParameters():
+    DefineManager.PLATFORM_INFO = sys.argv[1]
+    DefineManager.SEARCH_DEFAULT_COUNTRY = sys.argv[2]
+    DefineManager.SEARCH_DEFAULT_KEYWORD = sys.argv[3]
+    print "Input Parameters", DefineManager.PLATFORM_INFO, DefineManager.SEARCH_DEFAULT_COUNTRY, DefineManager.SEARCH_DEFAULT_KEYWORD
+
+def InitProgram():
+    if DefineManager.PLATFORM_INFO == "MAC":
+        DefineManager.CHROME_DEFAULT_BINARY_PATH = DefineManager.CHROME_MAC_PATH
+    elif DefineManager.PLATFORM_INFO == "WINDOWS":
+        DefineManager.CHROME_DEFAULT_BINARY_PATH = DefineManager.CHROME_WINDOWS_PATH
+    elif DefineManager.PLATFORM_INFO == "LINUX":
+        DefineManager.CHROME_LINUX_PATH = DefineManager.CHROME_LINUX_PATH
+    else:
+        DefineManager.CHROME_DEFAULT_BINARY_PATH = DefineManager.CHROME_MAC_PATH
+
+def GetNoticeParsedData(oneOfSelectedNoticeInfo):
+    noticeId = oneOfSelectedNoticeInfo.find_element_by_tag_name("th").text
+
+    noticeDetailData = oneOfSelectedNoticeInfo.find_elements_by_tag_name("td")
+    noticeTitle = noticeDetailData[DefineManager.TITLE_SAVED_POINT].text
+    noticeRedirectUrl = noticeDetailData[DefineManager.TITLE_SAVED_POINT].find_element_by_tag_name('a').get_attribute('href')
+    uploaderName = noticeDetailData[DefineManager.UPLOADER_SAVED_POINT].text
+    uploadDate = noticeDetailData[DefineManager.UPLOAD_DATE_SAVED_POINT].text
+    noticeShowedNum = noticeDetailData[DefineManager.PAGE_SHOWED_SAVED_POINT].text
+
+    return [noticeId,
+            noticeTitle,
+            noticeRedirectUrl,
+            uploaderName,
+            uploadDate,
+            noticeShowedNum]
+
+def CrawlArmyNoticeData():
+
+    chromeWebBrowser = webdriver.Chrome(DefineManager.CHROME_DEFAULT_BINARY_PATH)
+    chromeWebBrowser.get(DefineManager.CRAWL_TARGET_URL)
+    noticeList = chromeWebBrowser.find_element_by_xpath('//table//tbody')
+    noticeListDatas = noticeList.find_elements_by_xpath("tr")
+
+    for eachNoticeData in noticeListDatas:
+        eachNoticeString = eachNoticeData.text
+        if eachNoticeString.find(DefineManager.SEARCH_DEFAULT_COUNTRY) != DefineManager.NOT_AVAILABLE \
+                and eachNoticeString.find(DefineManager.SEARCH_DEFAULT_KEYWORD) != DefineManager.NOT_AVAILABLE:
+            parsedEachNoticeData = GetNoticeParsedData(eachNoticeData)
+            for indexOfNoticeData in parsedEachNoticeData:
+                print indexOfNoticeData
+    chromeWebBrowser.quit()
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-binaryPath = "./files/chromedriverMac"
+InitParameters()
+InitProgram()
+CrawlArmyNoticeData()
 
-chromeWebBrowser = webdriver.Chrome(binaryPath)
-
-chromeWebBrowser.get("http://mma.go.kr/board/boardList.do?mc=usr0000053&gesipan_id=16")
-noticeList = chromeWebBrowser.find_element_by_xpath('//table//tbody')
-noticeListDatas = noticeList.find_elements_by_xpath("tr")
-# print noticeListDatas
-count = 0
-for eachNoticeData in noticeListDatas:
-    count += 1
-    eachNoticeString = eachNoticeData.text
-    if eachNoticeString.find("서울") != -1 and eachNoticeString.find("사회복무요원") != -1:
-        print "#", count, eachNoticeString
-chromeWebBrowser.quit()
---
+++ Setting/DefineManager.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+NOT_AVAILABLE = -1
+CRAWL_TARGET_URL = "http://mma.go.kr/board/boardList.do?mc=usr0000053&gesipan_id=16"
+SEARCH_DEFAULT_COUNTRY = "서울"
+SEARCH_DEFAULT_KEYWORD = "사회복무요원"
+PLATFORM_INFO = "MAC"
+CHROME_DEFAULT_BINARY_PATH = "./files/chromedriverMac"
+CHROME_MAC_PATH = "./files/chromedriverMac"
+CHROME_WINDOWS_PATH = "./files/chromedriverWindows.exe"
+CHROME_LINUX_PATH = "./files/crhomedriverLinux"
+TITLE_SAVED_POINT = 0
+UPLOADER_SAVED_POINT = 1
+UPLOAD_DATE_SAVED_POINT = 3
+PAGE_SHOWED_SAVED_POINT = 4
